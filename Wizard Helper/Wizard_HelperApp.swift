0a50d7e8762b6716b7643ba761557d2453a305b6
Alexandra Zwinger <alexandra.zwinger@kurzdigital.com>
2025-09-17 10:40:05 +0200

    Initial Commit

---
+++ Wizard Helper/Wizard_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wizard_HelperApp.swift
+//  Wizard Helper
+//
+//  Created by Alexandra Zwinger on 17.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Wizard_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wizard HelperTests/Wizard_HelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wizard_HelperTests.swift
+//  Wizard HelperTests
+//
+//  Created by Alexandra Zwinger on 17.09.25.
+//
+
+import Testing
+@testable import Wizard_Helper
+
+struct Wizard_HelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
